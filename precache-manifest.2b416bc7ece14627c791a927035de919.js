56e8a5e3997d60d856c125dcf4bcff2ecb3992e6
unknown <thaer.rami99.alazzeh@gmail.com>
2020-08-13 11:35:38 +0300

    Updates

---
+++ precache-manifest.2b416bc7ece14627c791a927035de919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949a5984e51e517efb0dd431b99cb760",
+    "url": "/catsstore/index.html"
+  },
+  {
+    "revision": "fe79bf3d6c937bb8eda4",
+    "url": "/catsstore/static/css/main.51169569.chunk.css"
+  },
+  {
+    "revision": "5f8105ea769fc5f3b926",
+    "url": "/catsstore/static/js/2.7c364fe1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/catsstore/static/js/2.7c364fe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe79bf3d6c937bb8eda4",
+    "url": "/catsstore/static/js/main.6fe569b8.chunk.js"
+  },
+  {
+    "revision": "490bf897374abd2d07dc",
+    "url": "/catsstore/static/js/runtime-main.554c95fa.js"
+  }
+]);
